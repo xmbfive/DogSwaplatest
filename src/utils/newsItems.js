d3b7d643881c36de6cc313cbdbf6c27e10b0dcb9
xmbfive <152266905+xmbfive@users.noreply.github.com>
2026-06-20 23:04:29 +0600

    Update newsItems.js

--- src/utils/newsItems.js
+++ src/utils/newsItems.js
@@ -1,13 +1,13 @@
 const newsItems = [
-    "Visit our featured pool: https://dogswap.xyz/featured",
-    "The previous release is still available on https://old.dogswap.xyz",
-    "Does anyone know the location of John Connor?",
+    "Visit our featured pool: https://dog-swaplatest.vercel.app/featured",
+    "The previous release is still available on https://dog-swap-nine.vercel.app/",
+    "Does anyone know the location of Comatozze?",
     "The Bone Pools are active when payment is processed on MintMe!",
-    "Congratz DreadKnight on receiving a free featured spot!",
-    "Visit XatteR here: https://ancientbeast.com/",
+    "The stake and pools options are disabled",
     "Spotted an error, spelling mistake or bug? Shoot me a message!",
     "DogSwap is looking for active community members!",
     "WEIGHT means, the more, the better the rewards."
+    "Dogswap is again revived by libercat"
   ];
   
   export default newsItems;
